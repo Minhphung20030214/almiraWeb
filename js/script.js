6c10abf10ca9afa5ff4edf0421a6695752e40b44
Minhphung20030214 <minhphung140203@gmail.com>
2023-05-07 19:59:52 +0700

    new

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+
+// const formOpenbtn = document.querySelector("#form-open"),
+
+
+//     home = document.querySelector(".home"),
+//     formcontainer = document.querySelector(".form-container"),
+//     formClosebtn = document.querySelector(".form-close"),
+//     signupbtn = document.querySelector("#signup"),
+//     loginbtn= document.querySelector("#login"),
+//     pwshowhidebtn = document.querySelector(".pw-hide")
+
+// formOpenbtn.addEventListener("click", () => home.classList.add("show"));
+// formClosebtn.addEventListener("click", () => home.classList).remove("show");
+
+// signupbtn.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     formcontainer.classList.add("actice");
+// })
+// loginbtn.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     formcontainer.classList.remove("actice");
+// })
+// pwshowhidebtn.forEach(icon => {
+    
+// });
